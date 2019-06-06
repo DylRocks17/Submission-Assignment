175ab9faef31a890f19594802c8ddd3e7ec75e8a
DylRocks17 <40144819+DylRocks17@users.noreply.github.com>
2019-06-06 13:31:09 -0400

    Cloned and Wrote All Code for the Assignment

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -18,13 +18,38 @@
  *  - Add achiu-frhsd as a collaborator
  *
  */
-public class You{
+public class You
+{
 
 	private String firstName;
 	private String lastName;
 	private String favoriteAnimal;
 	private int age;
-
-	// Write your code here
+	
+	public You()
+	{
+		firstName = "Dylan";
+		lastName = "Ryan";
+		favoriteAnimal = "Bee";
+		age = 17;
+	}
+	
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public String setFavoriteAnimal(String animal)
+	{
+		favoriteAnimal = animal;
+		return favoriteAnimal;
+	}
+	
+	public int happyBirthday()
+	{
+		System.out.println("Happy Birthday");
+		age++;
+		return age;
+	}
 	
 }
